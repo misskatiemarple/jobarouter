7085177539614f9cf85b17d7178362e3e20830dc
misskatiemarple <misskatiemarple@gmail.com>
2023-08-14 17:12:28 +0900

    Set react router paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+//layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 function App() {
-  return <div className="App">Hello, Ninjas! changing test</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function home() {
+  return (
+    <div className="home">
+      <h1>Hello home</h1>
+      <p>text text tex</p>
+    </div>
+  );
+}
