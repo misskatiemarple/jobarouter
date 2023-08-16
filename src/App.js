dade83438a45835772d17698efdf7d6d0618744d
misskatiemarple <misskatiemarple@gmail.com>
2023-08-16 21:27:02 +0900

    Added help layout

--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ import About from "./pages/About";
 
 //layouts
 import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" />
+        <Route path="contact" />
+      </Route>
     </Route>
   )
 );
