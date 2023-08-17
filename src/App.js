e62d6b3a86f34adf1df304c7de5b78e60508ce9b
misskatiemarple <misskatiemarple@gmail.com>
2023-08-17 12:13:02 +0900

    custom error 404 added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
 
 //layouts
 import RootLayout from "./layouts/RootLayout";
@@ -27,6 +28,8 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+
+      <Route path="*" element={NotFound} />
     </Route>
   )
 );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Opps! Page not found.</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.{" "}
+      </p>
+      <p>
+        Go to the <Link to="/">Homepage</Link>.
+      </p>
+    </div>
+  );
+}
