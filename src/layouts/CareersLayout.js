09fe1cd7ddc19158f831dc9a682bb8ebb93077e8
misskatiemarple <misskatiemarple@gmail.com>
2023-08-23 15:51:59 +0900

    Debugged and HTML change

--- src/layouts/CareersLayout.js
+++ src/layouts/CareersLayout.js
@@ -5,12 +5,7 @@ export default function CareersLayout() {
   return (
     <div className="careers-layout">
       <h2>Careers</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat
-      </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
       <Outlet />
     </div>
   );
