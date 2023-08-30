7d49f3d940e04b8ff17f580039176a4ff36fb9ed
misskatiemarple <misskatiemarple@gmail.com>
2023-08-30 19:07:53 +0900

    User navigation added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
-export default function about() {
+export default function About() {
+  const [user, setUser] = useState("mario");
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <div className="about">
       <h1>About</h1>
@@ -15,6 +22,7 @@ export default function about() {
         dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 }
